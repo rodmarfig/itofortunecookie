67f243ada9c5ea85d059630060bdebc46417842f
Rodolfo <rodmarfig@gmail.com>
2016-10-20 17:38:12 -0500

    error 404

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 //exports.numero0=10;
 //exports.numero1 = 11;
+
 module.exports = {
     'IP' : process.env.IP || '127.0.0.1',
-    'PORT' : process.env.PORT || 3000
+    'PORT' : process.env.PORT || 3000,
+    'STATIC_PATH': './static' 
 };
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-// cargando modulo http
+// Cargando el modulo http
 var http = require('http');
-//Cargar la libreria colores
+// Cargando libreri path
+var path = require('path');
+// Cargando la libreria colors
 var colors = require('colors');
-//Generando un tema
+var fs = require('fs');
+// Generando un tema
 colors.setTheme({
   silly: 'rainbow',
   input: 'grey',
@@ -11,29 +14,58 @@ colors.setTheme({
   info: 'green',
   data: 'grey',
   help: 'cyan',
-  warn: 'yellow',
+  warn: ['yellow', 'bgWhite'],
   debug: 'blue',
-  error: 'red'
+  error: 'red',
+  achivement: 'rainbow'
 });
-//obteniendo configuraciones
-var config = require("./config/config");
-
-//console.log(`> contenido del modulo= ${modulo}`)
+// Obteniendo configruaciones
+var config = require('./config/config');
 var IP = config.IP;
 var PORT = config.PORT;
+
+var counter = 0;
 // Creando el server
 var server = http.createServer(function(req,res){
-    res.writeHead(
-        200,{
-            'Content-Type':'text/plain',
-            'server':'Buho@0.0.0'
+    var urlPath = req.url;
+    console.log(`> URL solicitada: ${urlPath}`.silly);
+    if(urlPath == '/'){
+        // Genera una ruta hacia el index.html
+        urlPath = path.resolve('./static/index.html');
+    }else{
+        // Genera una ruta dentro de static
+        urlPath = 
+        path.resolve(config.STATIC_PATH + urlPath);        
+    }
+    // Exratyendo la extencion de lo que
+    // vamos a servir
+    var extname = path.extname(urlPath);
+    // Seleccionar el contet-type con base
+    // en el extname
+    var contentType = 'text/plain';
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css'
+    };
+    fs.exists(urlPath,function(exists){
+        if(!exists){
+            // No  existe
+            res.writeHead(404,{
+                'Content-Type':'text/html'
+            });
+            res.end('<h1>404 NOT FOUND... :(</h1>');
+        }else{
+            // Si existe
+            res.end(`${urlPath} existe`);
         }
-    );
-
-res.write('Hola desde el server...');
-res.end();
+    });
 });
-// Poniendo a escuchar al server
-server.listen(PORT, IP, function(){ 
-    console.log("> server escuchando en ".info+`http://${IP}:${PORT}/ ...`.info);
+// Poniendo a escuchar
+// al server
+server.listen(PORT,IP,function(){
+    console.log("> Server escuchando en ".info +
+    `http://${IP}:${PORT}/ ...`.info);
 });
