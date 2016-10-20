d04dfd60cff0caa90d5740f32dfbfeac1f6e9e59
Rodolfo <rodmarfig@gmail.com>
2016-10-20 18:09:20 -0500

    Server statico completo

--- server.js
+++ server.js
@@ -23,12 +23,11 @@ colors.setTheme({
 var config = require('./config/config');
 var IP = config.IP;
 var PORT = config.PORT;
-
 var counter = 0;
 // Creando el server
 var server = http.createServer(function(req,res){
     var urlPath = req.url;
-    console.log(`> URL solicitada: ${urlPath}`.silly);
+    //console.log(`> URL solicitada: ${urlPath}`.silly);
     if(urlPath == '/'){
         // Genera una ruta hacia el index.html
         urlPath = path.resolve('./static/index.html');
@@ -37,13 +36,14 @@ var server = http.createServer(function(req,res){
         urlPath = 
         path.resolve(config.STATIC_PATH + urlPath);        
     }
-    // Exratyendo la extencion de lo que
-    // vamos a servir
+    // Extrayendo la extencion de lo que vamos a servir
     var extname = path.extname(urlPath);
-    // Seleccionar el contet-type con base
-    // en el extname
+    // Seleccionar el contet-type con base en el extname
     var contentType = 'text/plain';
     switch(extname){
+        case '.html':
+            contentType = 'text/html'
+            break;
         case '.js':
             contentType = 'text/javascript';
             break;
@@ -59,7 +59,22 @@ var server = http.createServer(function(req,res){
             res.end('<h1>404 NOT FOUND... :(</h1>');
         }else{
             // Si existe
-            res.end(`${urlPath} existe`);
+            //Leer archivo y servirlo
+            fs.readFile(urlPath, function(err, content){
+                if(err){
+                    res.writeHead(500, {
+                        'contentType':'text/html'
+                    });
+                    res.end('<h1> style="color:red>500 error</h1>"');
+                }else{
+                    //Si pudo leer el archivo!!
+                    res.writeHead(200,{
+                        'contentType': contentType
+                    });
+                    res.end(content);
+                }    
+            });
+            //res.end(`${urlPath} existe`);
         }
     });
 });
