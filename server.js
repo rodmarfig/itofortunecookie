04419db7fe82c3f6cf93f93f0a490ba042cca6d0
Rodolfo <rodmarfig@gmail.com>
2016-10-20 20:16:10 -0500

    Casi ultimo

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var http = require('http');
 // Cargando libreri path
 var path = require('path');
+// Cargando la libreria mime
+var mime = require('mime');
 // Cargando la libreria colors
 var colors = require('colors');
 var fs = require('fs');
@@ -23,11 +25,12 @@ colors.setTheme({
 var config = require('./config/config');
 var IP = config.IP;
 var PORT = config.PORT;
+
 var counter = 0;
 // Creando el server
 var server = http.createServer(function(req,res){
     var urlPath = req.url;
-    //console.log(`> URL solicitada: ${urlPath}`.silly);
+    console.log(`> URL solicitada: ${urlPath}`.silly);
     if(urlPath == '/'){
         // Genera una ruta hacia el index.html
         urlPath = path.resolve('./static/index.html');
@@ -36,20 +39,12 @@ var server = http.createServer(function(req,res){
         urlPath = 
         path.resolve(config.STATIC_PATH + urlPath);        
     }
-    // Extrayendo la extencion de lo que vamos a servir
+    // Exratyendo la extencion de lo que
+    // vamos a servir
     var extname = path.extname(urlPath);
-    // Seleccionar el contet-type con base en el extname
-    var contentType = 'text/plain';
-    switch(extname){
-        case '.html':
-            contentType = 'text/html'
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css'
-    };
+    // Seleccionar el contet-type con base
+    // en el extname
+    var contentType = mime.lookup(urlPath);
     fs.exists(urlPath,function(exists){
         if(!exists){
             // No  existe
@@ -59,22 +54,21 @@ var server = http.createServer(function(req,res){
             res.end('<h1>404 NOT FOUND... :(</h1>');
         }else{
             // Si existe
-            //Leer archivo y servirlo
-            fs.readFile(urlPath, function(err, content){
+            // Leemos el archivo y lo servimos
+            fs.readFile(urlPath,function(err, content){
                 if(err){
-                    res.writeHead(500, {
-                        'contentType':'text/html'
+                    res.writeHead(500,{
+                        'Content-Type':'text/html'
                     });
-                    res.end('<h1> style="color:red>500 error</h1>"');
+                    res.end('<h1 style="color: red">500 ERROR</h1>');                    
                 }else{
-                    //Si pudo leer el archivo!!
+                    // Si pudo leer el archivo
                     res.writeHead(200,{
-                        'contentType': contentType
+                        'Content-Type':contentType
                     });
                     res.end(content);
-                }    
+                }
             });
-            //res.end(`${urlPath} existe`);
         }
     });
 });
